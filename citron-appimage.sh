2274b538080810b9c9d05db6f5fd1f5ccbed3f1d
alexandre derumier <aderumier@odiso.com>
2025-03-16 19:39:42 +0100

    Update citron-appimage.sh
    test

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -71,68 +71,10 @@ fi
 	sudo ninja install
 	echo "$VERSION" >~/version
 )
-rm -rf ./citron
+
 VERSION="$(cat ~/version)"
 
-# NOW MAKE APPIMAGE
-mkdir ./AppDir
-cd ./AppDir
-
-cp -v /usr/share/applications/org.citron_emu.citron.desktop ./citron.desktop
-cp -v /usr/share/icons/hicolor/scalable/apps/org.citron_emu.citron.svg ./citron.svg
-ln -s ./citron.svg ./.DirIcon
-
-# Bundle all libs
-wget --retry-connrefused --tries=30 "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
-xvfb-run -a -- ./lib4bin -p -v -e -s -k \
-	/usr/bin/citron* \
-	/usr/lib/libGLX* \
-	/usr/lib/libGL.so* \
-	/usr/lib/libEGL* \
-	/usr/lib/dri/* \
-	/usr/lib/libvulkan* \
-	/usr/lib/libXss.so* \
-	/usr/lib/libdecor-0.so* \
-	/usr/lib/qt6/plugins/audio/* \
-	/usr/lib/qt6/plugins/bearer/* \
-	/usr/lib/qt6/plugins/imageformats/* \
-	/usr/lib/qt6/plugins/iconengines/* \
-	/usr/lib/qt6/plugins/platforms/* \
-	/usr/lib/qt6/plugins/platformthemes/* \
-	/usr/lib/qt6/plugins/platforminputcontexts/* \
-	/usr/lib/qt6/plugins/styles/* \
-	/usr/lib/qt6/plugins/xcbglintegrations/* \
-	/usr/lib/qt6/plugins/wayland-*/* \
-	/usr/lib/pulseaudio/* \
-	/usr/lib/pipewire-0.3/* \
-	/usr/lib/spa-0.2/*/* \
-	/usr/lib/alsa-lib/*
-
-# Prepare sharun
-#ln ./sharun ./AppRun
-#./sharun -g
-
-# turn appdir into appimage
 cd ..
-wget -q "$URUNTIME" -O ./uruntime
-chmod +x ./uruntime
+./appimage-builder.sh citron ./citron/build
 
-# Keep the mount point (speeds up launch time)
-sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime
-
-#Add udpate info to runtime
-echo "Adding update information \"$UPINFO\" to runtime..."
-./uruntime --appimage-addupdinfo "$UPINFO"
-
-echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S24 -B16 \
-	--header uruntime \
-	-i ./AppDir -o Citron-"$VERSION"-anylinux-"$ARCH".AppImage
-
-echo "Generating zsync file..."
-zsyncmake *.AppImage -u *.AppImage
 echo "All Done!"
